a2124c6b8a9b00b43ed473c1ab1faf90f5d80229
ecoffmaniv <ecoffmaniv@gmail.com>
2025-06-03 20:58:01 -0400

    Add files via upload

---
+++ DtoUbackup.ps1
@@ -0,0 +1,24 @@
+﻿powershell -ExecutionPolicy ByPass -NoExit {
+
+$source = "D:\Drive"
+$destination = "U:\Backups"
+
+$date = Get-Date -Format "MM-dd-yyyy HHmm"
+$backupname = "D Backup $date"
+$fullpath = Join-Path -Path $destination -ChildPath $backupname
+
+# Find existing backup. 
+$existing = Get-ChildItem -Path $destination -Filter "D Backup *" -ErrorAction SilentlyContinue
+
+# Delete old backup if exists. 
+if ($existing -ne $null) {
+    Write-Host "Deleting old backup: $($existing.FullName)"
+    $existing | Remove-Item -Recurse -Force -Verbose}
+
+# Copy files to backup. 
+try {
+    Copy-Item -Path $source -destination $fullpath -Recurse -Force
+    Write-Host "Backup Complete on $date" 
+} catch {
+    Write-Error "Backup Failed." }
+}
